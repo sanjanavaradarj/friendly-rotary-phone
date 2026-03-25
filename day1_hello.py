3093fa456420b22fcdb86107c285df0a1ca5a08d
sanjanavaradarj <sanjanavaradarj@gmail.com>
2026-03-25 18:40:10 +0530

    day 1 started python

---
+++ day1_hello.py
@@ -0,0 +1,4 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> print("hello, first day of python")
+hello, first day of python
