9b3a78fae3e6185630cb98bab9da2dc056d5870c
sanjanavaradarj <sanjanavaradarj@gmail.com>
2026-03-29 11:01:35 +0530

    day3_python

---
+++ day2_py.py
@@ -0,0 +1,23 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #accesing string characters
+>>> name = "ENGINEERING"
+>>> print(name[5])
+E
+>>> print(name[3])
+I
+>>> #sting slicing
+>>> name = "ENGINEERING"
+>>> name[2:6]
+'GINE'
+>>> name[1:10]
+'NGINEERIN'
+>>> #skip string
+>>> name = "ENGINEERING"
+>>> name[::6]
+'EE'
+>>> 
+>>> name[2::3]
+'GEI'
+>>> name[1::2]
+'NIERN'
