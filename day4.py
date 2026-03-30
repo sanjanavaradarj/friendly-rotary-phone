23aaad9463471a04eba6a3736fb82a16b05fdafd
sanjanavaradarj <sanjanavaradarj@gmail.com>
2026-03-30 14:30:40 +0530

    decision making

---
+++ day4.py
@@ -0,0 +1,26 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #elif
+>>> signal=input("wt is colour")
+wt is colouryellow
+>>> if signal=="red":
+...     print("stop")
+... elif signal=="yellow":
+...     print("ready")
+... else:
+...     print("go")
+... 
+...     
+ready
+>>> #largest of 2 numbers
+>>> a = int(input("enter first number"))
+enter first number22
+>>> b = int(input("enter secound number"))
+enter secound number12
+>>> if a > b:
+...     print("largest number is :",a)
+... else:
+...     print("largest number is :",b)
+... 
+...     
+largest number is : 22
